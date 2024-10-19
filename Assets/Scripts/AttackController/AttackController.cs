9a11d588b59412311d4bb5ae9e5fca0c7d7fa3b7
Kenny <lin.kenn@northeastern.edu>
2024-10-19 02:59:28 -0400

    point and click ability energy explosive

---
+++ Assets/Scripts/AttackController/AttackController.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using Unity.Burst.Intrinsics;
+using UnityEngine;
+using static UnityEditor.FilePathAttribute;
+
+namespace FantasyRpg.Combat
+{
+    public class AttackController : MonoBehaviour
+    {
+        private Animator animator;
+        public GameObject specialAttackOnePrefab;
+        public GameObject specialAttackTwoPrefab;
+        public AttributesManager atm;
+        private float _groundOffset = 0.75f;
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                animator.SetTrigger("BasicAttackOne");
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                animator.SetTrigger("SpecialAttackOne");
+            }
+            else if (Input.GetKeyDown(KeyCode.Q))
+            {
+                animator.SetTrigger("MeleeAttack");
+            }
+            StartCoroutine(SpecialAttackOne());
+            SpecialAttackTwo();
+        }
+
+        protected IEnumerator SpecialAttackOne()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Vector3 worldPosition = GetMouseWorldPosition();
+
+                // Add offset if the raycast hit the ground
+                if (worldPosition != Vector3.zero)
+                {
+                    worldPosition.y += _groundOffset;
+                }
+                animator.SetTrigger("SpecialAttackOne");
+                yield return new WaitForSeconds(0.5f);
+
+                GameObject animationInstance = Instantiate(specialAttackOnePrefab, worldPosition, Quaternion.identity);
+
+                StartCoroutine(ApplyAreaOfEffect(worldPosition, 3f, 5, 1f, (int)(atm.attack * 1.5f)));
+
+                Destroy(animationInstance, 5f);
+            }
+        }
+
+        protected void SpecialAttackTwo()
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Vector3 direction = (GetMouseWorldPosition() - transform.position).normalized;
+                Quaternion lookRotation = Quaternion.LookRotation(direction);
+
+                animator.SetTrigger("BasicAttackOne");
+
+                Vector3 startPosition = transform.position + transform.forward * 2f;
+
+                GameObject attackInstance = Instantiate(specialAttackTwoPrefab, startPosition, lookRotation);
+
+                Destroy(attackInstance, 2f);
+            }
+        }
+
+        private IEnumerator ApplyAreaOfEffect(Vector3 position, float radius, int duration, float interval, int tickDamage)
+        {
+            for (int i = 0; i < duration; i++)
+            {
+                Collider[] colliders = Physics.OverlapSphere(position, radius);
+                foreach (var other in colliders)
+                {
+                    if (other.CompareTag("Enemy"))
+                    {
+                        other.GetComponent<AttributesManager>().TakeDamage(tickDamage);
+                    }
+                }
+                yield return new WaitForSeconds(interval);
+            }
+        }
+
+        private IEnumerator ApplyDamageOverTime(Collider[] colliders, int duration, float interval, int tickDamage)
+        {
+            for (int i = 0; i < duration; i++)
+            {
+                foreach (var other in colliders)
+                {
+                    if (other.CompareTag("Enemy"))
+                    {
+                        other.GetComponent<AttributesManager>().TakeDamage(tickDamage);
+                    }
+                }
+                yield return new WaitForSeconds(interval);
+            }
+        }
+
+        private Vector3 GetMouseWorldPosition()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                return hit.point;
+            }
+            // Default to zero if no hit
+            return Vector3.zero; 
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterController/MovementDisabler.cs
@@ -0,0 +1,43 @@
+using FantasyRpg.Combat;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementDisabler : StateMachineBehaviour
+{
+    private WeaponManager weaponManager;
+    public bool enableMovement;
+    public bool enableRotation;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        weaponManager = animator.GetComponent<WeaponManager>();
+        weaponManager.EnableMovement(enableMovement);
+        weaponManager.EnableRotation(enableRotation);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
